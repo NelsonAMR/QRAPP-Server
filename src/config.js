e4091b1b7170c48200b0df45a827de45b42db017
NelsonAMR <nelson.macias99@gmail.com>
2024-03-09 21:57:29 -0600

    add logic to send data to app

--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ import "dotenv/config";
 export const SECRET_KEY = process.env.SECRET_KEY;
 export const PORT = process.env.PORT;
 export const DB_URI = process.env.DB_URI;
+export const ESP32_URI = process.env.ESP32_URI;
--- src/controllers/records.controller.js
+++ src/controllers/records.controller.js
@@ -1,5 +1,8 @@
 import Record from "../models/record.model.js";
 import Employee from "../models/employee.model.js";
+import { ESP32_URI } from "../config.js";
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const getRecords = async (req, res) => {
   try {
@@ -14,28 +17,54 @@ export const createRecord = async (req, res) => {
   const { employee_id } = req.body;
 
   try {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const timeoutId = setTimeout(() => controller.abort(), 3000);
+
     const employee = await Employee.findOne({ employee_id });
-
     if (!employee) {
+      await fetch(`${ESP32_URI}/?status=failure`, { signal });
+
       return res.status(404).json({ message: "Employee not found" });
+    } else {
+      await fetch(`${ESP32_URI}/?status=success`, { signal });
     }
 
-    const newRecord = await new Record({
-      employee: {
-        _id: employee._id,
-        name: employee.name,
-        email: employee.email,
-        employee_id: employee.employee_id,
-      },
-      date: new Date(),
-      entry: new Date(),
-    }).save();
+    clearTimeout(timeoutId);
 
-    if (!newRecord) {
-      return res.status(404).json({ message: "Record not found" });
+    const record = await Record.findOne({
+      "employee.employee_id": employee_id,
+    });
+
+    if (!record || record.departure.length > 0) {
+      const newRecord = await new Record({
+        employee: {
+          _id: employee._id,
+          name: employee.name,
+          email: employee.email,
+          employee_id: employee.employee_id,
+        },
+        date: new Date(),
+        entry: new Date(),
+      }).save();
+
+      if (!newRecord) {
+        return res.status(404).json({ message: "Record not found" });
+      }
+
+      return res.json({ message: "Record created successfully" });
     }
 
-    res.json({ message: "Record created successfully" });
+    if (!record.departure.length) {
+      await Record.findOneAndUpdate(
+        { "employee.employee_id": employee_id },
+        { departure: new Date() },
+        { new: true }
+      );
+
+      return res.json({ message: "Record updated successfully" });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
